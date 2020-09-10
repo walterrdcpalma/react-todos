a5f300d6ff6a751d1fd983838385375957825258
walterrdcpalma <walterrdcpalma@gmail.com>
2020-09-10 01:03:36 +0100

    Initial release

---
+++ react-todo/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./App.css";
+
+//Importing Components
+
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Walter's to do list</h1>
+      </header>
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
